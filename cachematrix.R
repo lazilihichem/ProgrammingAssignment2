a72285782fa44062977bf7b4aabf1a04a5cd2f94
lazili hicham <fh_lazili@esi.dz>
2020-06-05 23:21:35 +0100

    last commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,16 @@
 
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
+      ## the set function set the value of the matrix
       set <- function(y){
           y <<- x
           inv <<- NULL
       }
+      ## get the value of the matrix
       get <- function() x
+      ## this function set the value of the inverse 
       setinv <- function(y) inv <<- y
+      ## this function get the value of the inverse 
       getinv <- function() inv
       
       list(set = set , get = get , setinv = setinv , getinv = getinv)
@@ -21,12 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ##2 first we get the actual value of the inv
         inv <- x$getinv()
-        if(is.null(inv)){
-          inv <- solve(x$get())
-          x$setmean(inv)
+        ## if the inverse is not NULL
+        if(!is.null(inv)){
+          ## we return the inv if it's not null
           return(inv)
+          
         }
-          inv
+        ##we calculate the inverse then we return its value
+        inv <- solve(x$get())
+        x$setinv(inv)
+        inv
 }
 
